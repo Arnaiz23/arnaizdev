b067c2d8b84f731871676b080a95046b4b6d0f71
Arnaiz23 <aaf110501@gmail.com>
2023-01-15 15:16:02 +0100

    feat: 🎸 Add the pdf header to the curriculum download

--- pages/api/curriculum/index.js
+++ pages/api/curriculum/index.js
@@ -4,5 +4,6 @@ import { readFileSync } from "fs"
 export default (_, res) => {
   const path = join(process.cwd(), "./public/curriculum.pdf")
   const cv = readFileSync(path)
+  res.setHeader("Content-type", "application/pdf")
   res.send(cv)
 }
