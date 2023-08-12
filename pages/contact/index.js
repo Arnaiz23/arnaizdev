70af9f945a3cf7307e54c4fa5c95e467f000f1c9
Arnaiz23 <aaf110501@gmail.com>
2023-08-12 13:16:51 +0200

    feat: ✨ Create the design of the contact form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head"
-import ContactOption from "../../components/ContactOption"
-import Layout from "../../components/Layout"
-
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import ContactOption from "@/components/ContactOption"
+import Layout from "@/components/Layout"
+
+import styles from "@/styles/Contact.module.css"
 
 export default function Contact() {
   return (
@@ -12,9 +12,9 @@ export default function Contact() {
       <Head>
         <title>Arnaizdev - Contact</title>
       </Head>
-      <article>
-        <h2>Contacto y recursos</h2>
+      <section className={styles.main}>
         <section>
+          <h2 className={styles.title}>Recursos</h2>
           <ContactOption
             title="Arnaiz23"
             image={faGithub}
@@ -22,30 +22,36 @@ export default function Contact() {
             ariaLabel="GitHub link"
           />
           <ContactOption
-            title="adri.arnaizfernandez@gmail.com"
-            image={faEnvelope}
-            reverse
-            link="mailto:adri.arnaizfernandez@gmail.com"
-            ariaLabel="Gmail link"
-          />
-          <ContactOption
             title="Adrián Arnaiz Fernández"
             image={faLinkedin}
+            reverse
             link="https://es.linkedin.com/in/adri%C3%A1n-arnaiz-fern%C3%A1ndez-b67743227/en?trk=people-guest_people_search-card"
             ariaLabel="Linkedin link"
           />
         </section>
-      </article>
-      <style jsx>{`
-        article {
-          width: 100%;
-        }
-        article h2 {
-          text-align: center;
-          margin: 40px;
-          font-size: 30px;
-        }
-      `}</style>
+        <section>
+          <h2 className={styles.title}>Contacto</h2>
+          <form className={styles.formContainer}>
+            <div className={styles.row}>
+              <div className={styles.containerInput}>
+                <label>Correo</label>
+                <input type="email" placeholder="test@gmail.com" />
+              </div>
+              <div className={styles.containerInput}>
+                <label>Nombre</label>
+                <input type="text" placeholder="Adrián" />
+              </div>
+            </div>
+            <div className={styles.containerTextArea}>
+              <label>Mensaje</label>
+              <textarea placeholder="Escribe tu mensaje..."></textarea>
+            </div>
+            <button type="submit" className={styles.buttonSubmit}>
+              Enviar
+            </button>
+          </form>
+        </section>
+      </section>
     </Layout>
   )
 }
