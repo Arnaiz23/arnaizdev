86c4dc8656ce1bb33c2d18d6c608de1e0059b052
Arnaiz23 <aaf110501@gmail.com>
2024-03-08 20:51:53 +0100

    feat ✨: Control the errors when the user don't send the email, name and message

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,11 +1,34 @@
-import { EmailTemplate } from "@/components/EmailTemplate.js"
 import { Resend } from "resend"
+import z from "zod"
+
+import { EmailTemplate } from "@/components/EmailTemplate.js"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async (req, res) => {
+  const { email, name, message } = req.body
+
+  const schema = z.object({
+    email: z
+      .string({
+        required_error: "The email is required!!!",
+      })
+      .email(),
+    name: z.string({
+      required_error: "The name is required!!!",
+    }),
+    message: z.string({
+      required_error: "The message is required!!!",
+    }),
+  })
+
   try {
-    const { email, name, message } = req.body
+    schema.parse({ email, name, message })
+  } catch (error) {
+    return res.status(404).json(error.issues)
+  }
+
+  try {
     const data = await resend.emails.send({
       from: "Arnaizdev <noreply@arnaizdev.com>",
       to: [process.env.RESEND_TO_EMAIL],
@@ -13,8 +36,8 @@ export default async (req, res) => {
       react: EmailTemplate({ name, email, message }),
     })
 
-    res.status(200).json(data)
+    return res.status(200).json(data)
   } catch (error) {
-    res.status(400).json(error)
+    return res.status(400).json(error)
   }
 }
